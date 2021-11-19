3d327f7a32928bdb1832de54d5d487c34dcf37ac
Zrinka Aljinovic <aljinovic.zrinka@gmail.com>
2021-11-19 17:01:12 +0100

    redundancies removed from main class

--- src/main/java/com/typeqast/meterreadings/MeterReadingsApplication.java
+++ src/main/java/com/typeqast/meterreadings/MeterReadingsApplication.java
@@ -1,12 +1,10 @@
 package com.typeqast.meterreadings;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -22,10 +20,4 @@ public class MeterReadingsApplication {
 	DataSource deviceStatusDatasource() {
 		return DataSourceBuilder.create().build();
 	}
-
-//	@Bean("deviceDataTopicMaintainerNamedParamsJdbcTemplate")
-//	NamedParameterJdbcTemplate deviceDataTopicMaintainerNamedParamJdbcTemplate(
-//			@Qualifier("deviceDataTopicMaintainerDatasource") DataSource dataSource) {
-//		return new NamedParameterJdbcTemplate(dataSource);
-//	}
 }
